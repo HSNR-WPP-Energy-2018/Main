d49734bc15dde1adfef2fe4ff987d12a936eaa9c
SilverIceDragon <annavoigt94@outlook.de>
2018-11-29 00:13:01 +0100

    Started changing the data structure for Newton (Hashmaps instead of Arraylists) -> not finished yet

--- src/main/java/de/hsnr/wpp2018/algorithms/Newton.java
+++ src/main/java/de/hsnr/wpp2018/algorithms/Newton.java
@@ -5,10 +5,9 @@ import de.hsnr.wpp2018.Helper;
 import de.hsnr.wpp2018.Heuristics;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
 
 public class Newton implements Algorithm<Newton.Configuration> {
 
@@ -57,7 +56,7 @@ public class Newton implements Algorithm<Newton.Configuration> {
             p = Heuristics.castNegativesToZero(p);
         }
 
-        System.out.printf("Approximation for next x is " + "%f\n", p);
+        //System.out.printf("Approximation for next x is " + "%f\n", p);
         if (p == Double.POSITIVE_INFINITY)
         {
             //Heuristik ergänzen
@@ -69,18 +68,62 @@ public class Newton implements Algorithm<Newton.Configuration> {
         double P;
         //int size_of_neighbors = 10;
         ArrayList<Double> neighbors = new ArrayList<>();
+        TreeMap<LocalDateTime, Double> neighbors_map = new TreeMap<>();
+        Map<LocalDateTime, Double> tempmap = new LinkedHashMap<>();
+        Map<LocalDateTime, Double> neighbors_asc = new LinkedHashMap<>();
         Map.Entry<LocalDateTime, Double> entry = data.firstEntry();
         while (data.higherEntry(entry.getKey()) != null) {
             neighbors.add(entry.getValue());
+            neighbors_map.put(entry.getKey(), entry.getValue());
             LocalDateTime one = entry.getKey();
             LocalDateTime two = data.higherKey(entry.getKey());
-            neighbors.sort(Collections.reverseOrder()); //damit die k nächsten Nachbarn nicht später von der falschen Seite abgeschnitten werden
+
+
+            Map<LocalDateTime, Double> neighbors_desc = neighbors_map.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.<LocalDateTime, Double>comparingByValue().reversed())
+                    .collect(toMap(Map.Entry::getKey,
+                            Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+            //damit die k nächsten Nachbarn nicht später von der falschen Seite abgeschnitten werden
+
 
             if (Helper.getDistance(one, two) > configuration.getInterval()) {
                 if (neighbors.size() >= configuration.getNeighbors()) {
                     neighbors.subList(configuration.neighbors, neighbors.size()).clear();
+
+
+                    int counter = 0;
+                    for(Iterator<Map.Entry<LocalDateTime, Double>>it=neighbors_desc.entrySet().iterator();it.hasNext();){
+                        Map.Entry<LocalDateTime, Double> entry2 = it.next();
+                        if (counter >= configuration.neighbors) {
+                            it.remove();
+                        }
+                        counter++;
+                    }
+
                 }
+
                 Collections.sort(neighbors);
+                neighbors_desc.entrySet()
+                        .stream()
+                        .sorted(Map.Entry.comparingByValue())
+                        .forEachOrdered(x -> neighbors_asc.put(x.getKey(), x.getValue()));
+
+
+               // Zur Validierung
+                /*
+                for (int i=0; i<neighbors.size();i++)
+                {
+                    System.out.println(neighbors.get(i));
+                }
+                System.out.println("");
+                for (Map.Entry<LocalDateTime, Double> entry2 : neighbors_asc.entrySet()) {
+                    System.out.println("Key: " + entry2.getKey() + ". Value: " + entry2.getValue());
+                }
+                System.out.println("");
+                System.out.println("Ende");
+                System.out.println("");
+                */
 
                 int amount_of_x = 0;
                 for (LocalDateTime newDate = one.plusMinutes(15); newDate.isBefore(two); newDate = newDate.plusMinutes(15)) {
@@ -108,6 +151,10 @@ public class Newton implements Algorithm<Newton.Configuration> {
                     }
                 }
                 neighbors.clear();
+                neighbors_desc.clear();
+                neighbors_asc.clear();
+                neighbors_map.clear();
+                tempmap.clear();
             }
             entry = data.higherEntry(entry.getKey());
         }
