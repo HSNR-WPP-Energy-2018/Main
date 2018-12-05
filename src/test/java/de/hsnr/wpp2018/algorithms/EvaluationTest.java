39d8ab2ecc90bda5360748e01bf57f8641ba3bdf
Sebastian <sebastian.bair@googlemail.com>
2018-12-05 10:16:13 +0100

    Moved EvaluationTest to separate class

--- src/test/java/de/hsnr/wpp2018/algorithms/EvaluationTest.java
+++ src/test/java/de/hsnr/wpp2018/algorithms/EvaluationTest.java
@@ -1,8 +1,7 @@
 package de.hsnr.wpp2018.algorithms;
 
-import de.hsnr.wpp2018.Algorithm;
-import de.hsnr.wpp2018.Heuristics;
 import de.hsnr.wpp2018.Importer;
+import de.hsnr.wpp2018.RangeAdjuster;
 import de.hsnr.wpp2018.evaluation.Rating;
 import de.hsnr.wpp2018.evaluation.TestDataGenerator;
 import org.junit.After;
@@ -14,11 +13,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
-public class AlgorithmTest {
-    static final int INTERVAL = 15 * 60;
-    private static final int PERSONS = 2;
-    private static final double SIZE = 80.0;
-
+public class EvaluationTest {
     private static TreeMap<LocalDateTime, Double> original;
     private static TreeMap<LocalDateTime, Double> testData;
     private TreeMap<LocalDateTime, Double> result;
@@ -34,23 +29,13 @@ public class AlgorithmTest {
     }
 
     @Test
-    public void linear() {
-        result = new Linear().interpolate(testData, new Algorithm.Configuration(INTERVAL));
-    }
-
-    @Test
-    public void newton() {
-        result = new Newton().interpolate(testData, new Newton.Configuration(INTERVAL, 10));
-        Heuristics.useHeuristics(result, new Heuristics.Household(PERSONS, SIZE));
-    }
-
-    @Test
-    public void averaging() {
-        ArrayList<Averaging.ConfigurationInterval> intervals = new ArrayList<>();
-        intervals.add(new Averaging.ConfigurationInterval(5, (LocalDateTime current) -> current.plusWeeks(1), true, 5));
-        intervals.add(new Averaging.ConfigurationInterval(7, (LocalDateTime current) -> current.plusDays(1), true, 3));
-        intervals.add(new Averaging.ConfigurationInterval(15, (LocalDateTime current) -> current.plusSeconds(INTERVAL)));
-        result = new Averaging().interpolate(testData, new Averaging.Configuration(INTERVAL, intervals));
+    public void evaluateAveraging() {
+        ArrayList<RangeAdjuster> adjusters = new ArrayList<>();
+        adjusters.add((LocalDateTime current) -> current.plusWeeks(1));
+        adjusters.add((LocalDateTime current) -> current.plusDays(1));
+        Averaging.Configuration configuration = new Averaging.Optimizer(AlgorithmTest.INTERVAL, adjusters, 1, 10, 1, 10, 0.1).optimize(original, testData);
+        System.out.println("Configuration for best score: " + configuration);
+        result = new Averaging().interpolate(testData, configuration);
     }
 
     @After
