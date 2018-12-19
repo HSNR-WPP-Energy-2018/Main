fef1a0399581ec3c292b888ecc9f3dcf44856cff
SilverIceDragon <annavoigt94@outlook.de>
2018-12-19 19:53:33 +0100

    waste during daytime

--- src/main/java/de/hsnr/wpp2018/Optimizations/AvgNightDay.java
+++ src/main/java/de/hsnr/wpp2018/Optimizations/AvgNightDay.java
@@ -10,48 +10,88 @@ import java.util.ArrayList;
 public class AvgNightDay {
 
 
-    public static void dailyWaste(ArrayList<Algorithm.Consumption> newdata, Heuristics.Household household) {
+    public static void nightDayWaste(ArrayList<Algorithm.Consumption> newdata, Heuristics.Household household) {
         double meanDaily = Heuristics.average_waste_per_day(household);
         double meanHourly = meanDaily / 60;
+        Heuristics.Wastings wastings = new Heuristics.Wastings(meanHourly); //stündlicher Verbrauch
 
-        Heuristics.Wastings wastings = new Heuristics.Wastings(meanHourly);
-
-
-    }
-
-
-    public static void nocturnalWaste(ArrayList<Algorithm.Consumption> newdata, Heuristics.Household household) {
-        double meanDaily = Heuristics.average_waste_per_day(household);
-        double meanHourly = meanDaily / 60;
-        Heuristics.Wastings wastings = new Heuristics.Wastings(meanHourly);
-        double minTolerance = wastings.getProcessCooling();
-        double maxTolerance = wastings.getHeating() + wastings.getProcessCooling() + (wastings.getICT()/ 2);
+        double minNightTolerance = wastings.getProcessCooling();
+        double maxNightTolerance = wastings.getHeating() + wastings.getProcessCooling() + (wastings.getICT()/ 2);
         double avgNight = wastings.getHeating() + wastings.getProcessCooling();
+        double avgMorning = avgNight + wastings.getIllumination() + (wastings.getWarmWater()/6); //Licht + 10 min Duschen
+        double avgDay = wastings.getHeating() + wastings.getProcessCooling() + wastings.getICT() + wastings.getIllumination();
 
         LocalTime weekdayNightBegin = LocalTime.of(23, 00); //evtl aufpassen, falls Heuristik auf 23 Uhr gestellt wird -> betrachtet anderen Tag
         LocalTime weekdayNightEnd = LocalTime.of(07, 00);
         LocalTime weekendNightBegin = LocalTime.of(00, 00);
         LocalTime weekendNightEnd = LocalTime.of(9, 00);
+        LocalTime workingTimeEnd = LocalTime.of(18, 00);
+
 
         for (int i = 0; i < newdata.size(); i++) {
             LocalDateTime today = newdata.get(i).getTime();
-            //Verbrauch nachts an Werktagen
-            if (Heuristics.isBusinessDay(newdata.get(i).getTime())) {
-                if (!newdata.get(i).isInterpolated() && today.toLocalTime().isAfter(weekdayNightBegin) || today.toLocalTime().isBefore(weekdayNightEnd)) {
-                    if (newdata.get(i).getEnergyData() < minTolerance || newdata.get(i).getEnergyData() > maxTolerance) {
-                        newdata.get(i).setEnergyData(avgNight);
+
+            if (newdata.get(i).isInterpolated())
+            {
+                if (Heuristics.isBusinessDay(newdata.get(i).getTime())) {
+                    //Verbrauch nachts an Werktagen (hier muss ein "oder" hin, weil der Zähler nach 23 wieder auf 00 resettet wird
+                    if (today.toLocalTime().isAfter(weekdayNightBegin) || today.toLocalTime().isBefore(weekdayNightEnd)) {
+                        if (newdata.get(i).getEnergyData() < minNightTolerance || newdata.get(i).getEnergyData() > maxNightTolerance) {
+                            newdata.get(i).setEnergyData(avgNight);
+                        }
+                    }
+                    //Verbrauch morgens nach dem Aufstehen [ca. eine halbe Stunde zwischen Aufstehen und Haus-Verlassen]
+                    else if (today.toLocalTime().isAfter(weekdayNightEnd.minusMinutes(15)) && today.toLocalTime().isBefore(weekdayNightEnd.plusMinutes(45)))
+                    {
+                        //Morgendl.Verbrauch > als maxNightTolerance
+                        if (newdata.get(i).getEnergyData() < minNightTolerance || newdata.get(i).getEnergyData() > avgMorning) {
+                            newdata.get(i).setEnergyData(avgMorning);
+                        }
+                    }
+                    //Verbrauch an Werktagen, während Person außer Haus ist (sofern sie zur Schule/FH/Arbeit geht)
+                    else if (today.toLocalTime().isAfter(weekdayNightEnd.plusMinutes(30)) && today.toLocalTime().isBefore(workingTimeEnd))
+                    {
+                        //idR sind nur die gleichen Geräte wie nachts an, ggf. ist zusätzlich Heizung aus
+                        if (newdata.get(i).getEnergyData() < minNightTolerance || newdata.get(i).getEnergyData() > maxNightTolerance) {
+                            newdata.get(i).setEnergyData(avgNight);
+                        }
+                    }
+                    //Verbrauch nach Feierabend, ist grds. schwerer einzuschätzen, da hier meist mehr Peaks entstehen (nachträglich genauere Heuristiken einbauen)
+                    else
+                    {
+                        //Hier wird davon ausgegangen, dass die Person nach Feierabend immer da ist
+                        if (newdata.get(i).getEnergyData() < avgDay) {
+                            newdata.get(i).setEnergyData(avgDay);
+                        }
+                        else if (newdata.get(i).getEnergyData() > meanHourly) //Hier könnte ein Peak sein
+                        {
+                            newdata.get(i).setEnergyData(meanHourly);
+                        }
                     }
                 }
-            }
-            //Verbrauch nachts an Wochenenden
-            else {
-                if (!newdata.get(i).isInterpolated() && today.toLocalTime().isAfter(weekendNightBegin) || today.toLocalTime().isBefore(weekendNightEnd)) {
-                    if (newdata.get(i).getEnergyData() < minTolerance || newdata.get(i).getEnergyData() > maxTolerance) {
-                        newdata.get(i).setEnergyData(avgNight);
+                else
+                {
+                    //Verbrauch nachts an Wochenenden
+                    if (today.toLocalTime().isAfter(weekendNightBegin) && today.toLocalTime().isBefore(weekendNightEnd)) {
+                        if (newdata.get(i).getEnergyData() < minNightTolerance || newdata.get(i).getEnergyData() > maxNightTolerance) {
+                            newdata.get(i).setEnergyData(avgNight);
+                        }
+                    }
+                    else
+                    {
+                        //Hier wird davon ausgegangen, dass die Person am Wochenende auch mal weg ist (ähnl. Verbrauch wie nachts)
+                        if (newdata.get(i).getEnergyData() < minNightTolerance) {
+                            newdata.get(i).setEnergyData(minNightTolerance);
+                        }
+                        else if (newdata.get(i).getEnergyData() > meanHourly) //Hier könnte ein Peak sein
+                        {
+                            newdata.get(i).setEnergyData(meanHourly);
+                        }
                     }
                 }
             }
         }
+
     }
 
 
