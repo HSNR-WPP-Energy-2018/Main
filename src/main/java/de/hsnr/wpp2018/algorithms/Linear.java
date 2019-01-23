deb39c8d7ffd05fdb96c4979db4e1b1c1fa4117b
Sebastian <sebastian.bair@googlemail.com>
2019-01-23 11:07:25 +0100

    fix: use new object for linear algorithm to prevent modification of input data

--- src/main/java/de/hsnr/wpp2018/algorithms/Linear.java
+++ src/main/java/de/hsnr/wpp2018/algorithms/Linear.java
@@ -16,51 +16,47 @@ public class Linear implements Algorithm<Algorithm.Configuration> {
         return y1 + (x - x1) / (x2 - x1) * (y2 - y1);
     }
 
-    public TreeMap<LocalDateTime, Consumption> interpolate(TreeMap<LocalDateTime, Consumption> data, Configuration configuration) {
+    public TreeMap<LocalDateTime, Consumption> interpolate(TreeMap<LocalDateTime, Consumption> input, Configuration configuration) {
+        TreeMap<LocalDateTime, Consumption> data = new TreeMap<>(input);
+
         int decimals = 5;
         TreeMap<LocalDateTime, Consumption> values = new TreeMap<>();
         int counter = 1;
         double yLinear;
-
 
         LocalDateTime startDate = configuration.hasStart() ? configuration.getStart() : data.firstKey();
         LocalDateTime endDate = configuration.hasEnd() ? configuration.getEnd() : data.lastKey();
 
         Map.Entry<LocalDateTime, Consumption> entry = null;
         for (Map.Entry<LocalDateTime, Consumption> localEntry : data.entrySet()) {
-            if (localEntry.getKey().equals(startDate))
-            {
+            if (localEntry.getKey().equals(startDate)) {
                 entry = localEntry;
             }
         }
 
-        if(endDate.isAfter(data.lastKey()))
-        {
+        if (endDate.isAfter(data.lastKey())) {
             data.put(endDate.plusMinutes(15), new Consumption(-100.0, true));
         }
-
 
         while (!entry.getKey().isAfter(endDate) && data.higherEntry(entry.getKey()) != null) {
             LocalDateTime one = entry.getKey();
             LocalDateTime two = data.higherKey(entry.getKey());
             counter++;
-            if ((Helper.getDistance(one, two)/60) > configuration.getInterval()) {
-                if (data.get(two).getValue() != (-100.0)){
+            if ((Helper.getDistance(one, two) / 60) > configuration.getInterval()) {
+                if (data.get(two).getValue() != (-100.0)) {
 
-                //x1<=x<=x2s
-                yLinear = interpolateValue(counter, counter - 1, counter + 1, entry.getValue().getValue(), data.higherEntry(entry.getKey()).getValue().getValue());
-                yLinear = Helper.roundDouble(yLinear, decimals);
+                    //x1<=x<=x2s
+                    yLinear = interpolateValue(counter, counter - 1, counter + 1, entry.getValue().getValue(), data.higherEntry(entry.getKey()).getValue().getValue());
+                    yLinear = Helper.roundDouble(yLinear, decimals);
 
-                if (!values.containsKey(one)) {
-                    values.put(one, entry.getValue().copyAsOriginal());
-                }
+                    if (!values.containsKey(one)) {
+                        values.put(one, entry.getValue().copyAsOriginal());
+                    }
 
-                values.put(one.plusMinutes(configuration.getInterval()), new Consumption(yLinear, true));
-                data.put(one.plusMinutes(configuration.getInterval()), new Consumption(yLinear, true));
-                entry = data.higherEntry(one);
-            }
-            else
-                {
+                    values.put(one.plusMinutes(configuration.getInterval()), new Consumption(yLinear, true));
+                    data.put(one.plusMinutes(configuration.getInterval()), new Consumption(yLinear, true));
+                    entry = data.higherEntry(one);
+                } else {
                     System.out.println("Linear Interpolation cannot be used for predictions because if tries to find a curve within two known data points. Please use another interpolation algorithm.");
                     break;
                 }
