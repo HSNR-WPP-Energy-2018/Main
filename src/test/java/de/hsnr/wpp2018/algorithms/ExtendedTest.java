db0349b6aefb6a8e2b1f02be353e386cbcf05f38
Sebastian Bair <sebastian.bair@googlemail.com>
2019-01-27 20:08:57 +0100

    improved extended test

--- src/test/java/de/hsnr/wpp2018/algorithms/ExtendedTest.java
+++ src/test/java/de/hsnr/wpp2018/algorithms/ExtendedTest.java
@@ -6,7 +6,10 @@ import de.hsnr.wpp2018.database.*;
 import de.hsnr.wpp2018.evaluation.Rating;
 import de.hsnr.wpp2018.evaluation.TestDataGenerator;
 import de.hsnr.wpp2018.io.Importer;
-import de.hsnr.wpp2018.optimizations.*;
+import de.hsnr.wpp2018.optimizations.AvgNightDay;
+import de.hsnr.wpp2018.optimizations.Heuristics;
+import de.hsnr.wpp2018.optimizations.PatternRecognition;
+import de.hsnr.wpp2018.optimizations.SeasonalDifferences;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -58,40 +61,40 @@ public class ExtendedTest {
         Yesterday yesterday = new Yesterday();
         Yesterday.Configuration yesterdayConfiguration = new Yesterday.Configuration(AlgorithmTest.INTERVAL);
 
-        HashMap<String, ArrayList<Result>> results = new HashMap<>();
+        ArrayList<Result> results = new ArrayList<>();
         String[] algorithms = new String[]{Averaging.NAME, CubicSplines.NAME, DatabaseInterface.NAME, Linear.NAME, Newton.NAME, Yesterday.NAME};
-        for (String algorithm : algorithms) {
-            results.put(algorithm, new ArrayList<>());
-        }
 
-        for (float weekCut = 0.1f; weekCut <= 0.5f; weekCut += 0.1f) {
-            for (float dayCut = 0.1f; dayCut <= 0.5f; dayCut += 0.1f) {
-                for (float hourCut = 0.1f; hourCut <= 0.5f; hourCut += 0.1f) {
-                    for (float elementCut = 0.1f; elementCut <= 0.5f; elementCut += 0.1f) {
-                        System.out.println("Testing with weekCut=" + weekCut + ", dayCut=" + dayCut + ", hourCut=" + hourCut + ", elementCut=" + elementCut);
+        for (float weekCut = 0.0f; weekCut <= 0.5f; weekCut += 0.1f) {
+            for (float dayCut = 0.0f; dayCut <= 0.5f; dayCut += 0.1f) {
+                for (float hourCut = 0.0f; hourCut <= 0.5f; hourCut += 0.1f) {
+                    for (float elementCut = 0.0f; elementCut <= 0.5f; elementCut += 0.1f) {
+                        System.out.print("Testing with weekCut=" + weekCut + ", dayCut=" + dayCut + ", hourCut=" + hourCut + ", elementCut=" + elementCut + ": ");
                         TreeMap<LocalDateTime, Consumption> testData = new TestDataGenerator(original).cutRanges(weekCut, dayCut, hourCut, elementCut);
+                        HashMap<String, Double> differences = new HashMap<>();
 
-                        results.get(Averaging.NAME).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, averaging.interpolate(testData, averagingConfiguration))));
-                        results.get(CubicSplines.NAME).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, cubicSplines.interpolate(testData, cubicSplinesConfiguration))));
-                        results.get(DatabaseInterface.NAME).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, databaseInterface.interpolate(testData, databaseConfiguration))));
-                        results.get(Linear.NAME).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, linear.interpolate(testData, linearConfiguration))));
-                        results.get(Newton.NAME).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, newton.interpolate(testData, newtonConfiguration))));
-                        results.get(Yesterday.NAME).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, yesterday.interpolate(testData, yesterdayConfiguration))));
+                        differences.put(Averaging.NAME, Rating.calculateDifference(original, averaging.interpolate(new TreeMap<>(testData), averagingConfiguration)));
+                        differences.put(CubicSplines.NAME, Rating.calculateDifference(original, cubicSplines.interpolate(new TreeMap<>(testData), cubicSplinesConfiguration)));
+                        differences.put(DatabaseInterface.NAME, Rating.calculateDifference(original, databaseInterface.interpolate(new TreeMap<>(testData), databaseConfiguration)));
+                        differences.put(Linear.NAME, Rating.calculateDifference(original, linear.interpolate(new TreeMap<>(testData), linearConfiguration)));
+                        differences.put(Newton.NAME, Rating.calculateDifference(original, newton.interpolate(new TreeMap<>(testData), newtonConfiguration)));
+                        differences.put(Yesterday.NAME, Rating.calculateDifference(original, yesterday.interpolate(new TreeMap<>(testData), yesterdayConfiguration)));
+
+                        Result result = new Result(weekCut, dayCut, hourCut, elementCut, differences);
+                        System.out.println(result.toCSV(algorithms));
+                        results.add(result);
                     }
                 }
             }
         }
 
-        for (String algorithm : algorithms) {
-            StringBuilder builder = new StringBuilder();
-            builder.append(Result.HEADER).append("\n");
-            for (Result result : results.get(algorithm)) {
-                builder.append(result.toCSV()).append("\n");
-            }
-            PrintWriter printWriter = new PrintWriter(new File("out/evaluation-" + algorithm + ".csv"));
-            printWriter.write(builder.toString());
-            printWriter.close();
+        StringBuilder builder = new StringBuilder();
+        builder.append(Result.buildHeader(algorithms)).append("\n");
+        for (Result result : results) {
+            builder.append(result.toCSV(algorithms)).append("\n");
         }
+        PrintWriter printWriter = new PrintWriter(new File("out/evaluation-algorithms.csv"));
+        printWriter.write(builder.toString());
+        printWriter.close();
     }
 
     @Test
@@ -99,69 +102,76 @@ public class ExtendedTest {
         Newton newton = new Newton();
         Newton.Configuration newtonConfiguration = new Newton.Configuration(AlgorithmTest.INTERVAL, 10);
 
-        HashMap<String, ArrayList<Result>> results = new HashMap<>();
+        ArrayList<Result> results = new ArrayList<>();
         String[] heuristics = new String[]{
                 "heuristics_base",
                 Heuristics.NIGHT_DAY.toString(),
                 Heuristics.PATTERN.toString(),
                 Heuristics.SEASON.toString(),
         };
-        for (String heuristic : heuristics) {
-            results.put(heuristic, new ArrayList<>());
-        }
 
-        for (float weekCut = 0.1f; weekCut <= 0.5f; weekCut += 0.1f) {
-            for (float dayCut = 0.1f; dayCut <= 0.5f; dayCut += 0.1f) {
-                for (float hourCut = 0.1f; hourCut <= 0.5f; hourCut += 0.1f) {
-                    for (float elementCut = 0.1f; elementCut <= 0.5f; elementCut += 0.1f) {
+        for (float weekCut = 0.0f; weekCut <= 0.5f; weekCut += 0.1f) {
+            for (float dayCut = 0.0f; dayCut <= 0.5f; dayCut += 0.1f) {
+                for (float hourCut = 0.0f; hourCut <= 0.5f; hourCut += 0.1f) {
+                    for (float elementCut = 0.0f; elementCut <= 0.5f; elementCut += 0.1f) {
                         System.out.println("Testing with weekCut=" + weekCut + ", dayCut=" + dayCut + ", hourCut=" + hourCut + ", elementCut=" + elementCut);
                         TreeMap<LocalDateTime, Consumption> testData, temp;
                         testData = newton.interpolate(new TestDataGenerator(original).cutRanges(weekCut, dayCut, hourCut, elementCut), newtonConfiguration);
-                        results.get("heuristics_base").add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, testData)));
+                        HashMap<String, Double> differences = new HashMap<>();
+
+                        differences.put("heuristics_base", Rating.calculateDifference(original, testData));
 
                         temp = new TreeMap<>(testData);
                         AvgNightDay.nightDayWaste(temp, new Household(1, 150));
-                        results.get(Heuristics.NIGHT_DAY.toString()).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, temp)));
+                        differences.put(Heuristics.NIGHT_DAY.toString(), Rating.calculateDifference(original, temp));
 
                         temp = new TreeMap<>(testData);
                         PatternRecognition.checkBehaviour(temp, 4, 0.1);
-                        results.get(Heuristics.PATTERN.toString()).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, temp)));
+                        differences.put(Heuristics.PATTERN.toString(), Rating.calculateDifference(original, temp));
 
                         temp = new TreeMap<>(testData);
                         SeasonalDifferences.adjustSeasons(temp, true);
-                        results.get(Heuristics.SEASON.toString()).add(new Result(weekCut, dayCut, hourCut, elementCut, Rating.calculateDifference(original, temp)));
+                        differences.put(Heuristics.SEASON.toString(), Rating.calculateDifference(original, temp));
+
+                        results.add(new Result(weekCut, dayCut, hourCut, elementCut, differences));
                     }
                 }
             }
         }
 
-        for (String heuristic : heuristics) {
-            StringBuilder builder = new StringBuilder();
-            builder.append(Result.HEADER).append("\n");
-            for (Result result : results.get(heuristic)) {
-                builder.append(result.toCSV()).append("\n");
-            }
-            PrintWriter printWriter = new PrintWriter(new File("out/evaluation-" + heuristic + ".csv"));
-            printWriter.write(builder.toString());
-            printWriter.close();
+        StringBuilder builder = new StringBuilder();
+        builder.append(Result.buildHeader(heuristics)).append("\n");
+        for (Result result : results) {
+            builder.append(result.toCSV(heuristics)).append("\n");
         }
+        PrintWriter printWriter = new PrintWriter(new File("out/evaluation-heuristics.csv"));
+        printWriter.write(builder.toString());
+        printWriter.close();
     }
 
     private static class Result {
-        public static final String HEADER = "weekCut;dayCut;hourCut;elementCut;difference";
+        private static final String HEADER = "weekCut;dayCut;hourCut;elementCut";
+
+        public static String buildHeader(String[] keys) {
+            StringBuilder builder = new StringBuilder(HEADER);
+            for (String key : keys) {
+                builder.append(";").append(key);
+            }
+            return builder.toString();
+        }
 
         private float weekCut;
         private float dayCut;
         private float hourCut;
         private float elementCut;
-        private double difference;
+        private HashMap<String, Double> differences;
 
-        public Result(float weekCut, float dayCut, float hourCut, float elementCut, double difference) {
+        public Result(float weekCut, float dayCut, float hourCut, float elementCut, HashMap<String, Double> differences) {
             this.weekCut = weekCut;
             this.dayCut = dayCut;
             this.hourCut = hourCut;
             this.elementCut = elementCut;
-            this.difference = difference;
+            this.differences = differences;
         }
 
         public float getWeekCut() {
@@ -180,12 +190,23 @@ public class ExtendedTest {
             return elementCut;
         }
 
-        public double getDifference() {
-            return difference;
+        public HashMap<String, Double> getDifferences() {
+            return differences;
         }
 
-        public String toCSV() {
-            return weekCut + ";" + dayCut + ";" + hourCut + ";" + elementCut + ";" + difference;
+        public String toCSV(String[] keys) {
+            StringBuilder builder = new StringBuilder()
+                    .append(Float.toString(weekCut).replace('.', ','))
+                    .append(";")
+                    .append(Float.toString(dayCut).replace('.', ','))
+                    .append(";")
+                    .append(Float.toString(hourCut).replace('.', ','))
+                    .append(";")
+                    .append(Float.toString(elementCut).replace('.', ','));
+            for (String key : keys) {
+                builder.append(";").append(Double.toString(differences.getOrDefault(key, 0D)).replace('.', ','));
+            }
+            return builder.toString();
         }
     }
 }
